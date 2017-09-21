550fa49ef09c7177be329c2dd2c73cdb7f4e6d73
Mohammad-Ali Yaghoub-Zadeh-Fard <m.yaghubzade@gmail.com>
2017-09-21 11:55:43 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var APIBase = require("apibase-client");
+var client = new APIBase({
+    baseUrl: 'http://localhost:3000'
+});
+
+// Yooz SEARCH ID in APIBase
+var YOOZ_API = '70971c8b-c141-4c4c-8a1d-792fb5113493';
+module.exports = function(source, resourceType, cb) {
+  client.invokeOperation(YOOZ_API, source, null, null, function(err, result) {
+  
+      var searchResults = new Object();
+      searchResults.query = result["q"]
+      searchResults.results = []
+      result["results"].forEach( function (arrayItem)
+      {
+          var x = new Object()
+          x.title = arrayItem["title"]
+          x.snippet = arrayItem["snippet"]
+          searchResults.results.push(x)
+      }
+      cb(err, searchResults); 
+  });
+};
