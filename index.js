513da5d64a96556014063365ed7b5be73b2e4326
Mohammad-Ali Yaghoub-Zadeh-Fard <m.yaghubzade@gmail.com>
2017-09-21 12:37:43 +1000

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ var YOOZ_API = '70971c8b-c141-4c4c-8a1d-792fb5113493';
 module.exports = function(source, resourceType, cb) {
   client.invokeOperation(YOOZ_API, source, null, null, function(err, result) {
   
-      var searchResults = new Object();
+      var searchResults = {};
       searchResults.query = result["q"];
       searchResults.results = [];
       result["results"].forEach( function (arrayItem) {
-          var x = new Object();
+          var x = {};
           x.title = arrayItem["title"];
           x.snippet = arrayItem["snippet"];
           searchResults.results.push(x);
-      }
+      });
       cb(err, searchResults); 
   });
 };
